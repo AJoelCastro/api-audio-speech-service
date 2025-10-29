6dc451d339a7ea8f8bb06bd3de9e4a3a98631227
AJoelCastro <misthorn33@gmail.com>
2025-10-28 23:25:19 -0500

    first commit

---
+++ src/test/java/com/arturocastro/apiaudiospeechservice/ApiAudioSpeechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apiaudiospeechservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAudioSpeechServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
