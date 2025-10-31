d7ac88b708fb7ce4f23546d93f9314c2fd16dc8d
AJoelCastro <misthorn33@gmail.com>
2025-10-31 00:29:23 -0500

    service and webrtc

--- src/main/java/com/arturocastro/apiaudiospeechservice/service/AudioSpeechService.java
+++ src/main/java/com/arturocastro/apiaudiospeechservice/service/AudioSpeechService.java
@@ -1,10 +1,21 @@
 package com.arturocastro.apiaudiospeechservice.service;
 
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AudioSpeechService {
+    private final OpenAIClient openAIClient;
 
+    public AudioSpeechService(@Value("${openai.api.key}") String apiKey) {
+        openAIClient = new OpenAIOkHttpClient.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
 
+    // method here to create live session call
+    // public
 
 }
