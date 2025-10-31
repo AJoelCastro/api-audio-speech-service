99729aebedbafc89d9dce74988e8bae34b2248ee
AJoelCastro <misthorn33@gmail.com>
2025-10-31 00:16:22 -0500

    service and webrtc

--- src/main/java/com/arturocastro/apiaudiospeechservice/controller/AudioSpeechController.java
+++ src/main/java/com/arturocastro/apiaudiospeechservice/controller/AudioSpeechController.java
@@ -1,4 +1,12 @@
 package com.arturocastro.apiaudiospeechservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/openai/audio-speech")
 public class AudioSpeechController {
+
+
+
 }
