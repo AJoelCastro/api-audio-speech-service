fa3de0962e643de24bea1c8707f594e1b51d6165
AJoelCastro <misthorn33@gmail.com>
2025-11-01 00:58:45 -0500

    speech controller

--- src/main/java/com/arturocastro/apiaudiospeechservice/controller/AudioSpeechController.java
+++ src/main/java/com/arturocastro/apiaudiospeechservice/controller/AudioSpeechController.java
@@ -1,12 +1,29 @@
 package com.arturocastro.apiaudiospeechservice.controller;
 
+import com.arturocastro.apiaudiospeechservice.service.AudioSpeechService;
+import com.openai.core.http.HttpResponse;
+import com.openai.models.responses.ResponseOutputItem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpClient;
 
 @RestController
 @RequestMapping("/api/openai/audio-speech")
 public class AudioSpeechController {
 
+    private final AudioSpeechService audioSpeechService;
 
+    private AudioSpeechController(AudioSpeechService audioSpeechService) {
+        this.audioSpeechService = audioSpeechService;
+    }
+
+    @PostMapping("/session")
+    public ResponseEntity<HttpResponse> createSession(@RequestBody String clientSdp) throws Exception {
+        return ResponseEntity.ok(audioSpeechService.callService(clientSdp));
+    }
 
 }
--- src/main/java/com/arturocastro/apiaudiospeechservice/service/AudioSpeechService.java
+++ src/main/java/com/arturocastro/apiaudiospeechservice/service/AudioSpeechService.java
@@ -1,21 +1,48 @@
 package com.arturocastro.apiaudiospeechservice.service;
 
+import com.openai.core.http.HttpResponse;
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.audio.speech.SpeechCreateParams;
+import com.openai.models.audio.speech.SpeechModel;
+import com.openai.models.beta.realtime.ResponseAudioTranscriptDoneEvent;
+import com.openai.models.realtime.RealtimeResponse;
+import com.openai.models.realtime.RealtimeResponseCreateParams;
+import com.openai.models.responses.Response;
+import com.openai.models.responses.ResponseCreateParams;
+import com.openai.models.responses.ResponseOutputAudio;
+import dev.onvoid.webrtc.*;
+import dev.onvoid.webrtc.media.audio.AudioOptions;
+import dev.onvoid.webrtc.media.audio.AudioTrack;
+import dev.onvoid.webrtc.media.audio.AudioTrackSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.http.HttpClient;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AudioSpeechService {
-    private final OpenAIClient openAIClient;
 
+    private final OpenAIClient openAIClient;
+    private final PeerConnectionFactory factory;
     public AudioSpeechService(@Value("${openai.api.key}") String apiKey) {
         openAIClient = new OpenAIOkHttpClient.Builder()
                 .apiKey(apiKey)
                 .build();
+        factory = new PeerConnectionFactory();
+
     }
 
-    // method here to create live session call
-    // public
+    public HttpResponse callService (String clientSdp){
+        // Crear parámetros para Realtime
+        SpeechCreateParams speech = SpeechCreateParams.builder()
+                .model(SpeechModel.GPT_4O_MINI_TTS)
+                .input(clientSdp)
+                .voice(SpeechCreateParams.Voice.ASH)
+                .build();
+        return openAIClient.audio().speech().create(speech);
+    }
 
 }
